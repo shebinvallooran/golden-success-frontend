1e4a92310d842c371c01e63d279ba1d34f0daf91
Shebin Vallooran <shebinvallooran@gmail.com>
2025-09-12 01:43:12 +0530

    base url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://goldensuccessksa.com/apis/';
 
 const apiClient = axios.create({
   baseURL: `${API_BASE_URL}/api/v1`, // Replace with your API
